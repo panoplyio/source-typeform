7459847f11651bba9ca3ecb9d8fa097eb1b5b7a7
alonweissfeld <alon.weissfeld@panoply.io>
2016-12-01 11:58:57 +0200

    Fix versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="panoply_typeform",
-    version="1.0.1",
+    version="1.0.0",
     description="Panoply Data Source for the Typeform API",
     author="Alon Weissfeld",
     author_email="alon.weissfeld@panoply.io",
