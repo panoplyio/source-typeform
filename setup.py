d4471efb19b0f2e72e525dadcc8f2235aa33a39b
German <german@panoply.io>
2019-01-07 16:23:28 +0200

    Upgraded versions of the data source itself and of the  3rd perties.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 
 setup(
     name="panoply_typeform",
-    version="1.1.0",
+    version="1.2.0",
     description="Panoply Data Source for the Typeform API",
     author="Alon Weissfeld",
     author_email="alon.weissfeld@panoply.io",
     url="http://panoply.io",
     package_dir={"panoply": ""},
     install_requires=[
-        "panoply-python-sdk==1.5.0",
-        "requests==2.20.1",
-        "backoff==1.7.1",
-        "ratelimit==2.2.0"
+        "panoply-python-sdk==1.6.0",
+        "requests==2.21.0",
+        "backoff==1.8.0",
+        "ratelimit==2.2.1"
     ],
     extras_require={
         "test": [
